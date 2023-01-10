d6eb0038a53d4406a22bc7fbfc6f9a23287c0e02
Bhasim <bahaholding@aol.com>
2023-01-10 06:05:52 +0100

    update

--- src/App.js
+++ src/App.js
@@ -9,12 +9,31 @@ function App() {
   const [photos, setPhotos] = useState([]);
   const [success, setSuccess] = useState(false);
 
+  const [unit, setUnit] = useState('metric');
+  const [lat, setLat] = useState('');
+  const [lon, setLon] = useState('');
+  const [temp, setTemp] = useState(0);
+  const [iconId, setIconId] = useState('');
+  const [description, setDescription] = useState('');
+  const [windStatus, setWindStatus] = useState('');
+  const [airPressure, setAirPressure] = useState('');
+  
+
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      ifClicked();
+    }
+  }
+
+
  
 
   function ifClicked() {
     fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_KEY}&q=${locations}&days=5&aqi=no&alerts=no`
+      // `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.REACT_APP_KEY}&q=${locations}&days=5&aqi=no&alerts=no`
+      `https://api.openweathermap.org/data/2.5/weather?q=${locations}&appid=69644e28c6a9c6d7c04f95ff1035a799&units=${unit}`
     ) // https://home.openweathermap.org/api_keys (id) for the free weather api
+
       .then((res) => {
         if (res.ok) {
           console.log(res.status);
@@ -30,6 +49,14 @@ function App() {
       })
       .then((object) => {
         setWeather(object);
+        setTemp(Math.round(weather?.main?.temp));
+        setIconId(weather?.weather[0]?.icon);
+        setLat((weather?.coord.lat).toString());
+        setLon((weather.coord.lon).toString());
+        setDescription(weather?.weather[0]?.main);
+        setWindStatus((weather?.wind.speed).toFixed(1));
+        setAirPressure(weather?.main.pressure);
+       
         console.log(weather);
       })
       .catch((error) => console.log(error));
@@ -49,6 +76,7 @@ function App() {
       })
       .catch((error) => console.log(error));
   }
+ 
   return (
     <div className="app">
       <div className="wrapper">
@@ -59,31 +87,39 @@ function App() {
             onChange={(e) => setLocations(e.target.value)}
             placeholder="Enter location"
             className="location_input"
+            onKeyPress={handleKeyPress}
           />
-          <button className="location_searcher" onClick={ifClicked}>
+          <button className="location_searcher" onClick={ifClicked} onKeyPress={handleKeyPress}>
             Search Location
           </button>
+
         </div>
         {success ? (
           <>
             <div className="app__data">
               <div>
-                <img src={weather?.current?.condition?.icon} alt="wthr img" />
+
+                <img className="imagCondition" src={`http://openweathermap.org/img/wn/${iconId}@2x.png`} alt="Press Enter again"
+
+
+
+                />
                 <p className="temp">
-                  Condition: {weather?.current?.condition.text}{" "}
+                  Condition: {description}
+
                 </p>
               </div>
               <p className="temp">
                 Current Temperature:{" "}
-                {`${Math.floor(weather?.current?.temp_c)} °C`}
+                {`${Math.round(weather?.main?.temp)} °C`}
               </p>
-              <p className="temp">Humidity: {weather?.current?.humidity} %</p>
+              <p className="temp">Humidity: {weather?.main?.humidity} %</p>
 
               <p className="temp">
-                Sunrise:{weather?.forecast?.forecastday[0].astro.sunrise}
+              Wind Status: {windStatus} m/s
               </p>
               <p className="temp">
-                Sunset:{weather?.forecast?.forecastday[0].astro.sunset}
+                Air Pressure:  {airPressure} mb
               </p>
             </div>
             <img className="app_image" src={photos} alt="" />
